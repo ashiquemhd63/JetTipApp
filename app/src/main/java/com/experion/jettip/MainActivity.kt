60def66ba61331dceb650112944ed455d087c5b9
Muhammed Ashique <muhammedashiques2017@gmail.com>
2023-05-15 12:03:08 +0530

    Initial commit

---
+++ app/src/main/java/com/experion/jettip/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.experion.jettip
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.experion.jettip.ui.theme.JetTipTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+            // A surface container using the 'background' color from the theme
+            MyApp {
+                TopHeader()
+            }
+        }
+
+    }
+}
+
+
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    JetTipTheme {
+        Surface(
+
+            color = MaterialTheme.colorScheme.background
+        ) {
+            content()
+        }
+    }
+}
+@Preview
+@Composable
+fun TopHeader(totalPerPerson: Double = 0.0){
+    Surface(modifier = Modifier
+        .fillMaxWidth()
+        .height(150.dp)
+        .clip(shape = CircleShape.copy(all = CornerSize(12.dp))),
+        //.clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))),
+        color = Color(color = 0xFFE9D7F7)
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            val total = "%.2f".format(totalPerPerson)
+            Text(text = "Total Per Person",
+            style = MaterialTheme.typography.headlineSmall
+                )
+            Text(text = "$$total",
+                style = MaterialTheme.typography.headlineMedium,
+                fontWeight = FontWeight.ExtraBold
+                )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MainContent() {
+    Surface(modifier = Modifier
+        .padding(2.dp)
+        .fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(width = 1.dp, color= Color.LightGray)
+    ) {
+        Column() {
+
+        }
+    }
+}
