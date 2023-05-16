e3eeee446f6c436fad2348c11ab690dbf60fc1cf
Muhammed Ashique <muhammedashiques2017@gmail.com>
2023-05-16 10:09:51 +0530

    split par ui completed

--- app/src/main/java/com/experion/jettip/MainActivity.kt
+++ app/src/main/java/com/experion/jettip/MainActivity.kt
@@ -1,19 +1,29 @@
 package com.experion.jettip
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+//import androidx.compose.foundation.layout.BoxScopeInstance.align
+//import androidx.compose.foundation.layout.BoxScopeInstance.align
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Remove
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Shapes
@@ -34,6 +44,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.experion.jettip.components.InputField
 import com.experion.jettip.ui.theme.JetTipTheme
+import com.experion.jettip.widgets.RoundIconButton
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -65,13 +76,15 @@ fun MyApp(content: @Composable () -> Unit) {
         }
     }
 }
+
 @Preview
 @Composable
-fun TopHeader(totalPerPerson: Double = 0.0){
-    Surface(modifier = Modifier
-        .fillMaxWidth()
-        .height(150.dp)
-        .clip(shape = CircleShape.copy(all = CornerSize(12.dp))),
+fun TopHeader(totalPerPerson: Double = 0.0) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .clip(shape = CircleShape.copy(all = CornerSize(12.dp))),
         //.clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))),
         color = Color(color = 0xFFE9D7F7)
     ) {
@@ -81,13 +94,15 @@ fun TopHeader(totalPerPerson: Double = 0.0){
             verticalArrangement = Arrangement.Center
         ) {
             val total = "%.2f".format(totalPerPerson)
-            Text(text = "Total Per Person",
-            style = MaterialTheme.typography.headlineSmall
-                )
-            Text(text = "$$total",
+            Text(
+                text = "Total Per Person",
+                style = MaterialTheme.typography.headlineSmall
+            )
+            Text(
+                text = "$$total",
                 style = MaterialTheme.typography.headlineMedium,
                 fontWeight = FontWeight.ExtraBold
-                )
+            )
         }
     }
 }
@@ -96,32 +111,86 @@ fun TopHeader(totalPerPerson: Double = 0.0){
 @Preview
 @Composable
 fun MainContent() {
+    BillForm() { billAmt ->
+        Log.d("AMT", "MainContent: $billAmt")
+    }
+
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun BillForm(
+    modifier: Modifier = Modifier,
+    onValChange: (String) -> Unit = {}
+) {
     val totalBillState = remember {
         mutableStateOf("")
     }
     val validState = remember(totalBillState.value) {
         totalBillState.value.trim().isNotEmpty()
     }
-    val keyboardController =  LocalSoftwareKeyboardController.current
-    Surface(modifier = Modifier
-        .padding(2.dp)
-        .fillMaxWidth(),
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Surface(
+        modifier = Modifier
+            .padding(2.dp)
+            .fillMaxWidth(),
         shape = RoundedCornerShape(corner = CornerSize(8.dp)),
-        border = BorderStroke(width = 1.dp, color= Color.LightGray)
+        border = BorderStroke(width = 1.dp, color = Color.LightGray)
     ) {
-        Column() {
-            InputField(valueState = totalBillState,
+        Column(
+            modifier = Modifier.padding(6.dp),
+            verticalArrangement = Arrangement.Top
+        ) {
+            InputField(
+                valueState = totalBillState,
                 labelId = "Enter Bill",
                 enabled = true,
                 isSingleLine = true,
-                onAction = KeyboardActions{
+                onAction = KeyboardActions {
                     if (!validState) return@KeyboardActions
-
+                    //like callback
+                    onValChange(totalBillState.value.trim())
                     keyboardController?.hide()
+                }
+            )
+            if (validState) {
+                Row(
+//                    modifier = Modifier.padding(.dp),
+                    horizontalArrangement = Arrangement.Start,
+
+                    ) {
+                    Text(
+                        text = "Split",
+                        modifier = Modifier.align(
+                            alignment = Alignment.CenterVertically
+                        )
+                    )
+                    Spacer(modifier = Modifier.width(120.dp))
+                    Row(
+                        modifier = Modifier.padding(horizontal = 3.dp),
+                        horizontalArrangement = Arrangement.End
+
+                    ) {
+                        RoundIconButton(
+                            imageVector = Icons.Default.Remove ,
+                            onClick = { /*TODO*/ })
+
+                        Text(text = "2",
+                        modifier = Modifier.align(Alignment.CenterVertically).padding(start = 9.dp, end = 9.dp)
+                            )
+                        RoundIconButton(
+                            imageVector = Icons.Default.Add ,
+                            onClick = { /*TODO*/ })
+                    }
 
                 }
+            } else {
+                Box() {
 
-                )
+                }
+            }
         }
     }
+
 }
